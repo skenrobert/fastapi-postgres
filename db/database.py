ad4f6223b23c6482cd0dfb95b9e32e98b1b12dcf
kenny_robert <skenrobert@gmail.com>
2023-06-10 19:10:00 -0500

    update

--- db/database.py
+++ db/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from core.config import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:12345678@localhost:5432/fastapi"
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
